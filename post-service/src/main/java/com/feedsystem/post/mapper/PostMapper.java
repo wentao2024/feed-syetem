2a949dc5be27d486ff9cd238a049a7b6d6c545d4
wentao2024 <wentaoma@umass.edu>
2026-05-25 23:07:47 -0400

    '''

---
+++ post-service/src/main/java/com/feedsystem/post/mapper/PostMapper.java
@@ -0,0 +1,7 @@
+package com.feedsystem.post.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.feedsystem.post.entity.Post;
+
+public interface PostMapper extends BaseMapper<Post> {
+}
---
+++ user-service/src/main/java/com/feedsystem/user/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.feedsystem.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.feedsystem.user.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
