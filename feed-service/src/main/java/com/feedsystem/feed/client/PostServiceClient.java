d648aeec9e2e600e918c82b6ac2d78e2c5209aa3
Will Ma <160674270+wentao2024@users.noreply.github.com>
2026-08-03 22:54:59 -0400

    Update PostServiceClient.java

--- feed-service/src/main/java/com/feedsystem/feed/client/PostServiceClient.java
+++ feed-service/src/main/java/com/feedsystem/feed/client/PostServiceClient.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
 import java.util.List;
 
 @FeignClient(name = "post-service", fallback = PostServiceClientFallback.class)
