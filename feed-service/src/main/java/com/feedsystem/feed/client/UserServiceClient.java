c48401ed704ae5e5ab9ee856e869bdd836760ea3
Will Ma <160674270+wentao2024@users.noreply.github.com>
2026-08-03 22:55:55 -0400

    Update UserServiceClient.java

--- feed-service/src/main/java/com/feedsystem/feed/client/UserServiceClient.java
+++ feed-service/src/main/java/com/feedsystem/feed/client/UserServiceClient.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+
 import java.util.List;
 
 @FeignClient(name = "user-service", fallback = UserServiceClientFallback.class)
