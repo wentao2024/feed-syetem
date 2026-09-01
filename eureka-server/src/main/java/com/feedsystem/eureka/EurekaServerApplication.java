46e5a430cf05a57eb69a450697f359cd81f64fff
Will Ma <160674270+wentao2024@users.noreply.github.com>
2026-08-31 20:37:07 -0400

    更新 EurekaServerApplication.java

--- eureka-server/src/main/java/com/feedsystem/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/feedsystem/eureka/EurekaServerApplication.java
@@ -1,5 +1,6 @@
 package com.feedsystem.eureka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
